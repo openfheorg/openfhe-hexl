55eb27054e3b706b0d8131dd5d1b74f7bbcb4534
Jack Bates <jbates@dualitytech.com>
2022-03-24 10:52:36 -0600

    pulling in benchmark material

--- scripts/pull-hexl-from-openfhe-development.sh
+++ scripts/pull-hexl-from-openfhe-development.sh
@@ -6,13 +6,13 @@
 OPENFHE_SOURCE_REPO="../openfhe-development"
 OPENFHE_HEXL_DEST_REPO="."
 
-#DIR="benchmark/src"
-#FILES="lib-hexl-benchmark.cpp lib-hexl-util.h poly-hexl-benchmark-16k.cpp poly-hexl-benchmark-4k.cpp"
-#mkdir -p $OPENFHE_HEXL_DEST_REPO/$DIR                
-#for file in $FILES; do
-#  cp $OPENFHE_SOURCE_REPO/$DIR/$file $OPENFHE_HEXL_DEST_REPO/$DIR
-#  rm $OPENFHE_SOURCE_REPO/$DIR/$file
-#done
+DIR="benchmark/src"
+FILES="lib-hexl-benchmark.cpp lib-hexl-util.h poly-hexl-benchmark-16k.cpp poly-hexl-benchmark-4k.cpp"
+mkdir -p $OPENFHE_HEXL_DEST_REPO/$DIR                
+for file in $FILES; do
+  cp $OPENFHE_SOURCE_REPO/$DIR/$file $OPENFHE_HEXL_DEST_REPO/$DIR
+  rm $OPENFHE_SOURCE_REPO/$DIR/$file
+done
 
 DIR="src/core/include/lattice/hal/hexl"
 FILES="hexldcrtpoly.h lat-backend-hexl.h"
